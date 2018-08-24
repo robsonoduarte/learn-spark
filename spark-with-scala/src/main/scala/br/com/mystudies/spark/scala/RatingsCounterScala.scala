cf9203a1c5b1ecb138a1bdd054059599642582b0
robsonoduarte <robson.o.d@gmail.com>
2018-08-24 16:41:15 -0300

    add example with pure scala.

---
+++ spark-with-scala/src/main/scala/br/com/mystudies/spark/scala/RatingsCounterScala.scala
@@ -0,0 +1,17 @@
+package br.com.mystudies.spark.scala
+
+import scala.io.Source
+
+object RatingsCounterScala extends App {
+
+    Source.fromFile("F:/spark-scala-course/ml-100k/u.data")
+      .getLines()
+      .map(_.split("\t")(2))
+      .toSeq
+      .groupBy(id => id)
+      .mapValues(_.size)
+      .toSeq
+      .sortBy(_._1)
+      .foreach(println)
+
+}
