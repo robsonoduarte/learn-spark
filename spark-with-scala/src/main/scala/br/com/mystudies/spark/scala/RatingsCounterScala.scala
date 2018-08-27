e2a75b09f6fd70c6701702282da73c94114540f2
robsonoduarte <robson.o.d@gmail.com>
2018-08-27 13:57:32 -0300

    format code.

--- spark-with-scala/src/main/scala/br/com/mystudies/spark/scala/RatingsCounterScala.scala
+++ spark-with-scala/src/main/scala/br/com/mystudies/spark/scala/RatingsCounterScala.scala
@@ -3,7 +3,6 @@ package br.com.mystudies.spark.scala
 import scala.io.Source
 
 object RatingsCounterScala extends App {
-
     Source.fromFile("F:/spark-scala-course/ml-100k/u.data")
       .getLines()
       .map(_.split("\t")(2))
@@ -13,5 +12,4 @@ object RatingsCounterScala extends App {
       .toSeq
       .sortBy(_._1)
       .foreach(println)
-
 }
